f7cfe2d40441cf114860f6c07c8763f7fb321b57
Bill <wgarner001@student.butte.edu>
2017-01-31 23:07:12 +0000

    Lab1.1 submission

--- lab11/lab11.cpp
+++ lab11/lab11.cpp
@@ -1,12 +1,27 @@
 creat variable 1
 creat variable 2
 
-create randomizer character r
+create randomizer character r;
 //r is the computers randomization variable
-send character 'r' through randomizer function
+send character 'r' through randomizer function;
 
-create user guess character g 
+create user guess character g ;
 //g is the guess 
 
-request user imput 1 or 2
-set user input as 'g'
+create score character score s;
+//s is the score
+
+request user imput 1 or 2;
+set user input as 'g';
+//the guess is put it
+
+if r == g ;
+cout "You guessed right its " + r + " well Done! keep it up";
+
+if r != g;
+cout "nope! it was " + r + " too bad";
+    if g =< r;
+    cout "try a higher number";
+    if g => r;
+    cout "try a lower number";
+s - 1;
