eaf93c47ef4e6556c98586ecc668ff68b5785471
Bill <wgarner001@student.butte.edu>
2017-01-31 22:49:51 +0000

    Lab1.2 Commit

---
+++ lab11/lab11.cpp
@@ -0,0 +1,12 @@
+creat variable 1
+creat variable 2
+
+create randomizer character r
+//r is the computers randomization variable
+send character 'r' through randomizer function
+
+create user guess character g 
+//g is the guess 
+
+request user imput 1 or 2
+set user input as 'g'
---
+++ lab12/lab12.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+cout << "";
+cout << endl;
+cout << "||";
+cout << endl;
+cout << "|||";
+cout << endl;
+cout << "| ||                                                            [    ";
+cout << endl;
+cout << "|  ||                                                          []    ";
+cout << endl;
+cout << "|   ||                                                         []    ";
+cout << endl;
+cout << "|   ||                                                        [[]    ";
+cout << endl;
+cout << "|>>-||----->                                       >>---------[[]    ";
+cout << endl;
+cout << "|   ||                                                        [[]    ";
+cout << endl;
+cout << "|   ||                                                         []    ";
+cout << endl;
+cout << "|  ||                                                          []    ";
+cout << endl;
+cout << "| ||                                                            [    ";
+cout << endl;
+cout << "|||";
+cout << endl;
+cout << "||";
+cout << endl;
+cout << "";
+}
